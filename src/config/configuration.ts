578b79236b87cfb964b32f3f9812fcf7cdcdd535
Артем Садреев <artemsadreev@MacBook-Air-Artem.local>
2024-08-04 18:08:50 +0500

    add postgres, typeORM, usersmodule

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -7,6 +7,6 @@ export default () => ({
     port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
     user: process.env.POSTGRES_USER || 'balloon',
     password: process.env.POSTGRES_PASSWORD || '9Qpk2km4EC',
-    name: process.env.POSTGRES_DB || 'balloon_DB',
+    name: process.env.POSTGRES_DB || 'balloon_db',
   },
 });
